769d9abf16f1da1ab63c9c16a2c906147006230b
badri <badri.dilbert@gmail.com>
2009-08-02 09:42:57 +0530

    added the jquery parts

--- crosswordaddict/crosswords/views.py
+++ crosswordaddict/crosswords/views.py
@@ -14,15 +14,15 @@ def index(request):
             print cs_clues
             number_info = dict([(int(x[0]), {"code":x[1][:-1], "clue":x[2]}) for x in cs_clues])
             print number_info
-            crossword = {}
+            crossword = []
             for i,j in enumerate(grid):
                 if i+1 in number_info.keys():
-                    crossword[i+1] = {'grid':j, 'code': number_info[i+1]['code'], 'clue': number_info[i+1]['clue']}
+                    crossword.append({'grid':j, 'code': number_info[i+1]['code'], 'clue': number_info[i+1]['clue']})
                 else:
-                    crossword[i+1] = {'grid':j, 'code': '', 'clue': ''}
+                    crossword.append({'grid':j, 'code': '', 'clue': ''})
                     
             print crossword
-            return render_to_response('crossword.html', {'grid':grid})
+            return render_to_response('crossword.html', {'crossword':crossword})
         else:
             return render_to_response('crossword.html', {'error':True})
 
