07ab43dbb183ae95afed95cb5c1762be265168f0
badri <badri.dilbert@gmail.com>
2009-08-02 22:21:10 +0530

    preliminary working version, except the forms

--- crosswordaddict/crosswords/views.py
+++ crosswordaddict/crosswords/views.py
@@ -12,17 +12,20 @@ def index(request):
             crossword_id = CsCrossword.objects.filter(gridid=grid_id).values_list('id')
             cs_clues = CsClues.objects.filter(crosswordid=crossword_id).values_list('square', 'code', 'clue')
             print cs_clues
-            number_info = dict([(int(x[0]), {"code":x[1][:-1], "clue":x[2]}) for x in cs_clues])
-            print number_info
+            number_info = dict([(int(x[0]), {"code":x[1][:-1]}) for x in cs_clues])
+            across = [({"square":int(x[0]), "code":x[1][:-1], "clue":x[2]}) for x in cs_clues if x[1][-1]=='A']
+            down = [({"square":int(x[0]), "code":x[1][:-1], "clue":x[2]}) for x in cs_clues if x[1][-1]=='D']           
+            print across
+            print down
             crossword = []
             for i,j in enumerate(grid):
                 if i+1 in number_info.keys():
-                    crossword.append({'grid':j, 'code': number_info[i+1]['code'], 'clue': number_info[i+1]['clue']})
+                    crossword.append({'grid':j, 'code': number_info[i+1]['code']})
                 else:
-                    crossword.append({'grid':j, 'code': '', 'clue': ''})
+                    crossword.append({'grid':j, 'code': ''})
                     
             print crossword
-            return render_to_response('crossword.html', {'crossword':crossword})
+            return render_to_response('crossword.html', {'crossword':crossword, 'across': across, 'down':down})
         else:
             return render_to_response('crossword.html', {'error':True})
 
