d4fae61daadb020d649d6ea012ba61e3a3dd545f
Badri <badri.dilbert@gmail.com>
2012-12-10 08:31:27 +0530

    heroku ready

--- crosswordaddict/settings.py
+++ crosswordaddict/settings.py
@@ -86,3 +86,8 @@ INSTALLED_APPS = (
     'crosswords',
     'favorites',
 )
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
