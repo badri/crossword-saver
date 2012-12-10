0d22c3814d54230fa8a0101befb39e7e59f92b1f
Badri <badri.dilbert@gmail.com>
2012-12-10 08:11:45 +0530

    unused files cleanup

--- CSErrorMessages.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define( '001', 'That page cannot be accessed directly. Please use the menu.' );
-define( '002', 'Settings have been updated.' );
-define( '003', 'Your changes have been saved.' );
-define( '004', 'The selected crossword has been deleted.' );
-
-?>
